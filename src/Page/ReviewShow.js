af8602475bfd69d311410a7ca1a445f330c28e59
unknown <sifatniloy18@gmail.com>
2022-12-07 16:56:14 -0600

    star rating showing unsuccessful

--- src/Page/ReviewShow.js
+++ src/Page/ReviewShow.js
@@ -1,29 +1,13 @@
 import React, { useState } from 'react';
 import { Rating } from 'react-simple-star-rating';
 
-const ReviewShow = () => {  
-    const star=2;
-    const [rating, setRating] = useState(3)
+const ReviewShow = () => {
+    const stars = 4;
 
-    // Catch Rating value
-    const handleRating = (rate) => {
-        setRating(rate)        
-    }
-    // handleRating();
-    // Optinal callback functions
-    const onPointerEnter = () => console.log('Enter')
-    const onPointerLeave = () => console.log('Leave')
-    const onPointerMove = (value ,index) => console.log(value, index)
 
     return (
-        <div className='App'>
-            <Rating
-                onClick={handleRating}
-                // onPointerEnter={onPointerEnter}
-                // onPointerLeave={onPointerLeave}
-                onPointerMove={onPointerMove}
-            /* Available Props */
-            />
+        <div >
+            <h2>stars: </h2>
         </div>
     );
 };
